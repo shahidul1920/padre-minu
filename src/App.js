56aff5bb0801cd36312be4aa3e25fa3da6202775
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-01-02 20:15:08 +0600

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 
 
 const Pizza = () => {
-  return React.createElement("h1", {}, "fuck man");
+  return React.createElement("h1", {}, "fugg man");
 };
 
 const App = () => {
   return React.createElement("div", {}, [
+    React.createElement(Pizza),
     React.createElement(Pizza),
   ]);
 };
