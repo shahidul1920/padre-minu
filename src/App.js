f8c788a2d08d4f9b1683541c769df1243473e978
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-01-02 13:00:35 +0600

    before start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 const Pizza = (props) => {
-    return React.createElement('h1', {}, props.name);
-}
+  return React.createElement("h1", {}, props.name);
+};
 
 const App = () => {
-    return React.createElement('div', {}, [
-        React.createElement(Pizza, {name: "shakil's pizza"}),
-        React.createElement(Pizza, {name: "cheezy pizza"}),
-    ]);
-}
+    debugger;
+  return React.createElement("div", {}, [
+    React.createElement(Pizza, { name: "shakil's pizza" }),
 
-const containter = document.getElementById('root');
+    React.createElement(Pizza, { name: "cheezy pizza" }),
+  ]);
+};
+
+const containter = document.getElementById("root");
 const root = ReactDOM.createRoot(containter);
 root.render(React.createElement(App));
